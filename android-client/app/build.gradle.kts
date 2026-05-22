ae86c64866c4212bd6cbf7040f635d62c4b7ea59
Christian van Dijk <christian.vandijk@cito.nl>
2026-05-22 08:12:05 +0200

    🔖 chore: bump version to 2.6.3

--- android-client/app/build.gradle.kts
+++ android-client/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "nl.muorg.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20602
-        versionName = "2.6.2"
+        versionCode = 20603
+        versionName = "2.6.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
