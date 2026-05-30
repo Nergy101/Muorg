0febb1b29201ad526a9f8b2770c5844acfcd7f4b
Christian van Dijk <christian.vandijk@cito.nl>
2026-05-30 12:04:26 +0200

    🔖 chore: bump version to 2.15.0

--- android-client/app/build.gradle.kts
+++ android-client/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "nl.muorg.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21406
-        versionName = "2.14.6"
+        versionCode = 21500
+        versionName = "2.15.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
