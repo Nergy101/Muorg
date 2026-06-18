6f8d91c793856ff078fb5ac0889d08933fcaf653
Christian van Dijk <cdijk4@proton.me>
2026-06-18 23:04:05 +0200

    🔖 chore: bump version to 2.17.14

--- android-client/app/build.gradle.kts
+++ android-client/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "nl.muorg.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21711
-        versionName = "2.17.11"
+        versionCode = 21714
+        versionName = "2.17.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
