91fd9308d6a4b7c61e471fe758970fd8f31602f1
Christian van Dijk <cdijk4@proton.me>
2026-08-05 19:55:51 +0200

    🔖 chore: bump version to 2.33.1

--- android-client/app/build.gradle.kts
+++ android-client/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "nl.muorg.android"
         minSdk = 26
         targetSdk = 37
-        versionCode = 23300
-        versionName = "2.33.0"
+        versionCode = 23301
+        versionName = "2.33.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
