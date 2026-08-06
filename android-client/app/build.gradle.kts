ff62f20cdd07790eafcc1c3776540fc95ab90217
Christian van Dijk <cdijk4@proton.me>
2026-08-06 12:55:44 +0200

    🔖 chore: bump version to 2.34.8

--- android-client/app/build.gradle.kts
+++ android-client/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "nl.muorg.android"
         minSdk = 26
         targetSdk = 37
-        versionCode = 23407
-        versionName = "2.34.7"
+        versionCode = 23408
+        versionName = "2.34.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
