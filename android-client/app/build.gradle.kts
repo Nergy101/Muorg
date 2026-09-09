9d0659b490fb5a65795401c80937607b2327cbb2
Christian van Dijk <cdijk4@proton.me>
2026-09-09 17:43:29 +0200

    🔖 chore(android): bump version to 3.0.2

--- android-client/app/build.gradle.kts
+++ android-client/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "nl.muorg.android"
         minSdk = 26
         targetSdk = 37
-        versionCode = 30001
-        versionName = "3.0.1"
+        versionCode = 30002
+        versionName = "3.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
